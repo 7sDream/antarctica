bf0726c2861c08a9948c3575465f79a0282e759e
7sDream <7seconddream@gmail.com>
2018-03-24 17:47:51 +0800

    Revert "update dist public asset path"
    This reverts commit b2f0a8123bec7c54ee7a3804ea275843294dcf18.

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/antarctica/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
